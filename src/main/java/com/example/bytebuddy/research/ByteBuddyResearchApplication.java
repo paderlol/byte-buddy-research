cb02404cc5131a455f22498f49d0bdcc81677c7d
pader.zhang <pader.zhang@backstopdevelopment.com>
2018-06-22 17:16:55 +0800

    Initial commit

---
+++ src/main/java/com/example/bytebuddy/research/ByteBuddyResearchApplication.java
@@ -0,0 +1,12 @@
+package com.example.bytebuddy.research;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteBuddyResearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteBuddyResearchApplication.class, args);
+    }
+}
